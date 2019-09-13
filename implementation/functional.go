d8c773366ab6c1bf0976f65c1c9a7fd22cf04925
Gram <master_fess@mail.ru>
2019-09-13 14:11:34 +0200

    add map and reduce

--- implementation/functional.go
+++ implementation/functional.go
@@ -4,12 +4,29 @@ package implementation
 type T struct{}
 
 // Filter returns slice of T for which F returned true
-func Filter(f func(t T) bool, t []T) []T {
-	result := make([]T, 0, len(t))
-	for _, e := range t {
+func Filter(f func(el T) bool, arr []T) []T {
+	result := make([]T, 0, len(arr))
+	for _, e := range arr {
 		if f(e) {
 			result = append(result, e)
 		}
 	}
 	return result
+}
+
+// Map applies F to all elements in slice of T and returns slice of results
+func Map(f func(el T) T, arr []T) []T {
+	result := make([]T, 0, len(arr))
+	for _, e := range arr {
+		result = append(result, f(e))
+	}
+	return result
+}
+
+// Reduce applies F to acc and every element in slice of T and returns acc
+func Reduce(f func(el T, acc T) T, arr []T, acc T) T {
+	for _, el := range arr {
+		acc = f(el, acc)
+	}
+	return acc
 }
