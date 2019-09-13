cd865cf934ffa6a2ca0abd5e8ad0845566d99df3
Gram <master_fess@mail.ru>
2019-09-13 14:20:26 +0200

    test map

--- genesis_test.go
+++ genesis_test.go
@@ -17,3 +17,13 @@ func TestFilterInt32(t *testing.T) {
 	f(filterPositive, []int32{1, 2, 3}, []int32{1, 2, 3})
 	f(filterPositive, []int32{-1, -2, -3}, []int32{})
 }
+
+func TestMapInt32(t *testing.T) {
+	f := func(mapper func(t int32) int32, given []int32, expected []int32) {
+		actual := MapInt32(mapper, given)
+		assert.Equal(t, expected, actual, "they should be equal")
+	}
+	double := func(t int32) int32 { return t * 2 }
+
+	f(double, []int32{1, 2, 3}, []int32{2, 4, 6})
+}
