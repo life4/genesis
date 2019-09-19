679c0bf6dce56264bcbba28c74fa839c13eac741
Gram <master_fess@mail.ru>
2019-09-19 11:06:58 +0200

    test Slice

---
+++ implementation/slice_test.go
@@ -0,0 +1,36 @@
+package implementation
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChunkEvery(t *testing.T) {
+	f := func(count int, given []T, expected [][]T) {
+		actual := Slice{given}.ChunkEvery(count)
+		assert.Equal(t, expected, actual, "they should be equal")
+	}
+	f(2, []T{1, 2, 3, 4}, [][]T{[]T{1, 2}, []T{3, 4}})
+	f(2, []T{1, 2, 3, 4, 5}, [][]T{[]T{1, 2}, []T{3, 4}, []T{5}})
+}
+
+func TestFilter(t *testing.T) {
+	f := func(filter func(t T) bool, given []T, expected []T) {
+		actual := Slice{given}.Filter(filter)
+		assert.Equal(t, expected, actual, "they should be equal")
+	}
+	filterPositive := func(t T) bool { return t > 0 }
+
+	f(filterPositive, []T{1, -1, 2, -2, 3, -3}, []T{1, 2, 3})
+	f(filterPositive, []T{1, 2, 3}, []T{1, 2, 3})
+	f(filterPositive, []T{-1, -2, -3}, []T{})
+}
+
+func TestIntersperse(t *testing.T) {
+	f := func(el T, given []T, expected []T) {
+		actual := Slice{given}.Intersperse(el)
+		assert.Equal(t, expected, actual, "they should be equal")
+	}
+	f(0, []T{1, 2, 3}, []T{1, 0, 2, 0, 3})
+}
