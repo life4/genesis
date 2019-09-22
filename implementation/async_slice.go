4f1d4c4af19087ac25e56247d6cf6b9fbce51b65
Gram <master_fess@mail.ru>
2019-09-22 13:59:22 +0200

    TestAsyncSliceAll

--- implementation/async_slice.go
+++ implementation/async_slice.go
@@ -11,6 +11,69 @@ type AsyncSlice struct {
 	workers int
 }
 
+// All returns true if f returns true for all elements in slice
+func (s AsyncSlice) All(f func(el T) bool) bool {
+	if len(s.data) == 0 {
+		return true
+	}
+
+	wg := sync.WaitGroup{}
+
+	worker := func(jobs <-chan int, result chan<- bool, ctx context.Context) {
+		// enter
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case index, ok := <-jobs:
+				if !ok {
+					return
+				}
+				if !f(s.data[index]) {
+					result <- false
+					return
+				}
+			}
+		}
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	// when we're returning the result, cancel all workers
+	defer cancel()
+
+	// calculate workers count
+	workers := s.workers
+	if workers == 0 || workers > len(s.data) {
+		workers = len(s.data)
+	}
+
+	// run workers
+	jobs := make(chan int, len(s.data))
+	result := make(chan bool, workers)
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		go worker(jobs, result, ctx)
+	}
+
+	// close the result channel when all workers have done
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	// schedule the jobs: indices to check
+	for i := 0; i < len(s.data); i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for range result {
+		return false
+	}
+	return true
+}
+
 // Any returns true if f returns true for any element from slice
 func (s AsyncSlice) Any(f func(el T) bool) bool {
 	if len(s.data) == 0 {
--- implementation/async_slice_test.go
+++ implementation/async_slice_test.go
@@ -23,6 +23,24 @@ func TestAsyncSliceAny(t *testing.T) {
 	f(isEven, []T{1, 3, 5, 7, 9, 12}, true)
 }
 
+func TestAsyncSliceAll(t *testing.T) {
+	f := func(check func(t T) bool, given []T, expected bool) {
+		s := AsyncSlice{data: given, workers: 2}
+		actual := s.All(check)
+		assert.Equal(t, expected, actual, "they should be equal")
+	}
+	isEven := func(t T) bool { return (t % 2) == 0 }
+
+	f(isEven, []T{}, true)
+	f(isEven, []T{1}, false)
+	f(isEven, []T{1, 3}, false)
+	f(isEven, []T{2}, true)
+	f(isEven, []T{2, 4}, true)
+	f(isEven, []T{2, 3}, false)
+	f(isEven, []T{2, 4, 6, 8, 10, 12}, true)
+	f(isEven, []T{2, 4, 6, 8, 10, 11}, false)
+}
+
 func TestAsyncSliceMap(t *testing.T) {
 	f := func(mapper func(t T) G, given []T, expected []G) {
 		s := AsyncSlice{data: given, workers: 2}
