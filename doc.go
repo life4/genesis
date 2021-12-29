d5f66e9ff916b2973a432c7d8eeef2347051b6a8
gram <mail@orsinium.dev>
2021-12-29 09:17:16 +0100

    fix docs

--- doc.go
+++ doc.go
@@ -1,2 +1,10 @@
 // Package genesis provides generic functions for collections.
 package genesis
+
+// required for being discovered by pkg.go.dev
+import (
+	_ "github.com/life4/genesis/channels"
+	_ "github.com/life4/genesis/lambdas"
+	_ "github.com/life4/genesis/maps"
+	_ "github.com/life4/genesis/slices"
+)
