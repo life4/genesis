d63b67ef920f388566a0528a760cb070bc826730
gram <git@orsinium.dev>
2023-08-10 09:35:42 +0200

    decouple context-aware functions

--- channels/channel.go
+++ channels/channel.go
@@ -2,7 +2,6 @@ package channels
 
 import (
 	"context"
-	"reflect"
 	"sync"
 
 	"github.com/life4/genesis/constraints"
@@ -50,53 +49,6 @@ func BufferSize[T any](c <-chan T) int {
 
 func ChunkEvery[T any](c <-chan T, count int) chan []T {
 	return ChunkEveryC(context.Background(), c, count)
-}
-
-// ChunkEveryC returns channel with slices containing count elements each.
-//
-// ⏹️ Internally, the function starts a goroutine.
-// This goroutine finishes when the input channel is closed.
-// The returned channel is closed when this goroutine finishes.
-//
-// 🐞 BUG: The goroutine might not be cleaned up if
-// the input channel is closed but the goroutine is blocked
-// in attempt to write into the output channel.
-// To avoid the issue, make sure to consume all messages
-// from the output channel. In a future release, the function
-// might be changed to accept a context for better cancelation.
-//
-// ⏸️ The returned channel is unbuffered.
-// The goroutine will be blocked and won't consume elements
-// from the input channel until the value from the output channel
-// is consumed by another goroutine.
-func ChunkEveryC[T any](ctx context.Context, c <-chan T, count int) chan []T {
-	chunks := make(chan []T, 1)
-	go func() {
-		defer close(chunks)
-		chunk := make([]T, 0, count)
-		i := 0
-		for el := range WithContext(c, ctx) {
-			chunk = append(chunk, el)
-			i++
-			if i%count == 0 {
-				i = 0
-				select {
-				case chunks <- chunk:
-				case <-ctx.Done():
-					return
-				}
-				chunk = make([]T, 0, count)
-			}
-		}
-		if len(chunk) > 0 {
-			select {
-			case chunks <- chunk:
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
-	return chunks
 }
 
 // Close safely closes the given channel.
@@ -211,91 +163,6 @@ func First[T any](cs ...<-chan T) (T, error) {
 	return FirstC(context.Background(), cs...)
 }
 
-// FirstC selects the first available element from the given channels.
-//
-// The function returns in one of the following cases:
-//
-//  1. One of the given channels is closed. In this case,
-//     [ErrClosed] is returned.
-//  2. ⏹️ The ctx context is canceled. In this case,
-//     the cancelation reason is returned as an error.
-//  3. One of the given channels returns a value. In this case,
-//     the error is nil.
-//
-// If all channels are non-closed and empty and ctx is not canceled,
-// the function will block and wait for one of the above to occur.
-//
-// If multiple messages are received at the same time,
-// only one is chosen via a uniform pseudo-random selection (see below).
-// Other messages will stay in their queues untocuhed.
-//
-// In no scenario any messages are lost. A message is either
-// returned by a function or stays in the queue.
-//
-// # 😱 Errors
-//
-//   - [ErrEmpty]: no channels are passed into the function.
-//   - [ErrClosed]: a channel was closed.
-//   - Another: cancelation cause returned by ctx.Err().
-//
-// # 🍽 What is starvation
-//
-// Imagine that you iterate through a list of channels and return an element
-// from the first one that is available. Since the order of channels is always
-// the same, if a value is always available in a multiple channels, you'll be
-// selecting only from the first one. In other words, you never select from other
-// channels as long as the first one always has a value for you. It might result
-// in a situation when out of multiplt jobs you start only one is not blocked.
-//
-// This situation is called "[starvation]". To avoid that, the element in
-// select statement, according to the [Go specification],
-// "is chosen via a uniform pseudo-random selection".
-//
-// This function preserves that behavior by using internally
-// a dynamically created (using [reflect]) select statement.
-//
-// [starvation]: https://en.wikipedia.org/wiki/Starvation_(computer_science)
-// [Go specification]: https://go.dev/ref/spec#Select_statements
-func FirstC[T any](ctx context.Context, cs ...<-chan T) (T, error) {
-	// try to use a regular select if a small number of channels is passed
-	switch len(cs) {
-	case 0:
-		return *new(T), ErrEmpty
-	case 1:
-		select {
-		case v, ok := <-cs[0]:
-			if !ok {
-				return *new(T), ErrClosed
-			}
-			return v, nil
-		case <-ctx.Done():
-			return *new(T), ctx.Err()
-		}
-	}
-
-	cases := make([]reflect.SelectCase, 0, len(cs)+1)
-	cases = append(cases, reflect.SelectCase{
-		Dir:  reflect.SelectRecv,
-		Chan: reflect.ValueOf(ctx.Done()),
-	})
-	for _, c := range cs {
-		cases = append(cases, reflect.SelectCase{
-			Dir:  reflect.SelectRecv,
-			Chan: reflect.ValueOf(c),
-		})
-	}
-	chosen, rval, ok := reflect.Select(cases)
-	if chosen == 0 {
-		var v T
-		return v, ctx.Err()
-	}
-	val := rval.Interface().(T)
-	if !ok {
-		return val, ErrClosed
-	}
-	return val, nil
-}
-
 // Given a channel of channels of values, return a channel of values.
 //
 // This pattern is described in the "Concurrency in Go" book
@@ -399,57 +266,6 @@ func Merge[T any](cs ...<-chan T) chan T {
 	return MergeC(context.Background(), cs...)
 }
 
-// MergeC merges multiple channels into one.
-//
-// The order in which elements are merged from different channels
-// is not guaranteed.
-//
-// ⏹️ Internally, the function starts a goroutine per input channel.
-// Each of these goroutines finishes either when their input channel is closed.
-// or when the ctx context is cancelled.
-// The returned channel is closed when all of these goroutines finish.
-//
-// ⏸️ The returned channel is unbuffered.
-// The goroutines will be blocked and won't consume elements
-// from the input channels until the value from the output channel
-// is consumed by another goroutine.
-func MergeC[T any](ctx context.Context, cs ...<-chan T) chan T {
-	res := make(chan T)
-
-	// make sure the result channel is closed
-	// when all input channels are closed
-	wg := sync.WaitGroup{}
-	wg.Add(len(cs))
-	go func() {
-		wg.Wait()
-		close(res)
-	}()
-
-	echo := func(c <-chan T) {
-		wg.Done()
-		for {
-			select {
-			case v, ok := <-c:
-				if !ok {
-					return
-				}
-				select {
-				case res <- v:
-				case <-ctx.Done():
-					return
-				}
-			case <-ctx.Done():
-				return
-			}
-		}
-	}
-
-	for _, c := range cs {
-		go echo(c)
-	}
-	return res
-}
-
 // Min returns the minimal element from channel.
 //
 // ⏹️ The function is blocked until the channel is closed.
@@ -466,44 +282,6 @@ func Min[T constraints.Ordered](c <-chan T) (T, error) {
 		}
 	}
 	return min, nil
-}
-
-// Pop reads a value from the channel (with context).
-//
-// The function is blocking. It will wait and return
-// in one of the following conditions:
-//
-//  1. ⏹️ The context is canceled.
-//  2. ⏹️ The channel is closed.
-//  3. There is a value pushed into the channel.
-//
-// In the first two cases, the second return value (called "more" or "ok") is "false".
-// Otherwise, if a value is succesfully pulled from the channel, it is "true".
-//
-// Reads from nil channels block forever. So, if a nil channel is passed,
-// the function will exit only when the context is canceled.
-func Pop[T any](ctx context.Context, c <-chan T) (T, bool) {
-	select {
-	case v, more := <-c:
-		return v, more
-	case <-ctx.Done():
-		return *new(T), false
-	}
-}
-
-// Push writes the value into the channel (with context).
-//
-// ⚠️ Experimental! Behavior of this function might change in the future
-// or the function can be removed altogether.
-// It's not clear yet what's the best approach for when the target channel is closed.
-// By default, Go panics in this case, which might be not good in some situations.
-// Also, using this function might cause situations when the canceled context
-// will be ignored by the target function instead of exiting.
-func Push[T any](ctx context.Context, c chan<- T, v T) {
-	select {
-	case c <- v:
-	case <-ctx.Done():
-	}
 }
 
 // Reduce applies f to acc and every element from channel and returns acc.
@@ -665,48 +443,6 @@ func WithBuffer[T any](c <-chan T, bufSize int) chan T {
 		defer close(result)
 		for el := range c {
 			result <- el
-		}
-	}()
-	return result
-}
-
-// WithContext creates an echo channel of the given one that can be canceled with ctx.
-//
-// This can be useful in 2 scenarios:
-//
-//  1. To be able to cancel any function in this package
-//     without closing the original channel.
-//  2. For simpler iteration through channels with support for cancellation.
-//     This pattern is descirbed in the "Concurrency in Go" book
-//     in "The or-done-channel" chapter (page 119).
-//
-// ⏹️ Internally, the function starts a goroutine
-// that copies values from the input channel into the output one.
-// This goroutine finishes when the input channel is closed
-// or the ctx context is canceled.
-// The returned channel is closed when this goroutine finishes.
-//
-// ⏸️ The returned channel is unbuffered.
-// The goroutine will be blocked and won't consume elements
-// from the input channel until the value from the output channel
-// is consumed by another goroutine.
-func WithContext[T any](c <-chan T, ctx context.Context) chan T {
-	result := make(chan T)
-	go func() {
-		defer close(result)
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case val, more := <-c:
-				if !more {
-					return
-				}
-				select {
-				case result <- val:
-				case <-ctx.Done():
-				}
-			}
 		}
 	}()
 	return result
--- channels/channel_test.go
+++ channels/channel_test.go
@@ -2,7 +2,6 @@ package channels_test
 
 import (
 	"context"
-	"sync"
 	"testing"
 
 	"github.com/life4/genesis/channels"
@@ -212,115 +211,6 @@ func TestFirst(t *testing.T) {
 	is.Equal(v, 42)
 }
 
-func TestFirstC(t *testing.T) {
-	t.Parallel()
-	is := is.New(t)
-	ctx := context.Background()
-	f := func(n, i int) {
-		csW := make([]chan<- int, n)
-		csR := make([]<-chan int, n)
-		for i := 0; i < n; i++ {
-			c := make(chan int)
-			csW[i] = c
-			csR[i] = c
-		}
-		go func() { csW[i] <- 12 }()
-		v, err := channels.FirstC(ctx, csR...)
-		is.NoErr(err)
-		is.Equal(v, 12)
-	}
-	for n := 1; n < 10; n++ {
-		for i := 0; i < n; i++ {
-			f(n, i)
-		}
-	}
-
-	_, err := channels.FirstC[int](ctx)
-	is.Equal(err, channels.ErrEmpty)
-}
-
-func TestFirstC_Cancellation(t *testing.T) {
-	t.Parallel()
-	is := is.New(t)
-	ctx := context.Background()
-
-	// one closed channel
-	c1 := make(chan int)
-	close(c1)
-	_, err := channels.FirstC(ctx, c1)
-	is.Equal(err, channels.ErrClosed)
-
-	// two channels, one closed
-	c2 := make(chan int)
-	_, err = channels.FirstC(ctx, c2, c1)
-	is.Equal(err, channels.ErrClosed)
-
-	// one channel, context cancelled on wait
-	ctx2, cancel := context.WithCancel(context.Background())
-	cancel()
-	_, err = channels.FirstC(ctx2, c2)
-	is.Equal(err, context.Canceled)
-
-	// two channels, context cancelled on wait
-	c3 := make(chan int)
-	c4 := make(chan int)
-	ctx3, cancel := context.WithCancel(context.Background())
-	cancel()
-	_, err = channels.FirstC(ctx3, c3, c4)
-	is.Equal(err, context.Canceled)
-}
-
-func TestFirstC_Starvation(t *testing.T) {
-	t.Parallel()
-	is := is.New(t)
-
-	nChannels := 50
-	nMessages := 50
-	bufSize := 2
-
-	// create channels
-	csW := make([]chan<- int, nChannels)
-	csR := make([]<-chan int, nChannels)
-	for i := 0; i < nChannels; i++ {
-		c := make(chan int, bufSize)
-		csW[i] = c
-		csR[i] = c
-	}
-
-	// For each channel, start a job that will emit
-	// in the channel this channel's ID.
-	ctx, cancel := context.WithCancel(context.Background())
-	for i := 0; i < nChannels; i++ {
-		go func(i int) {
-			for m := 0; m < nMessages; m++ {
-				select {
-				case csW[i] <- i:
-				case <-ctx.Done():
-					return
-				}
-			}
-		}(i)
-	}
-
-	// Calculate how many messages are received from each channel.
-	nReceived := make(map[int]uint32)
-	for k := 0; k < nChannels*4; k++ {
-		i, err := channels.FirstC(context.Background(), csR...)
-		is.NoErr(err)
-		nReceived[i] += 1
-	}
-
-	// Check if there are any starved channels.
-	// A starved channel is the one that couldn't get
-	// any messages selected from it.
-	cancel()
-	for i, n := range nReceived {
-		if n == 0 {
-			t.Errorf("channel %d is starved (n=%d)", i, n)
-		}
-	}
-}
-
 func TestFlatten(t *testing.T) {
 	is := is.New(t)
 	chIn := make(chan (<-chan int))
@@ -431,54 +321,6 @@ func TestMerge(t *testing.T) {
 	is.True(!more)
 }
 
-func TestMergeC(t *testing.T) {
-	is := is.New(t)
-
-	// no channels passed
-	ctx := context.Background()
-	c1 := channels.MergeC[int](ctx)
-	_, more := <-c1
-	is.True(!more)
-
-	// passed channels are all closed
-	c2 := make(chan int)
-	c3 := make(chan int)
-	close(c2)
-	close(c3)
-	c4 := channels.MergeC(ctx, c2, c3)
-	_, more = <-c4
-	is.True(!more)
-
-	// one channel
-	c5 := make(chan int)
-	go func() {
-		c5 <- 42
-		close(c5)
-	}()
-	c6 := channels.MergeC(ctx, c5)
-	v := <-c6
-	is.Equal(v, 42)
-	_, more = <-c6
-	is.True(!more)
-
-	// cancel on read
-	ctx2, cancel := context.WithCancel(context.Background())
-	c7 := channels.MergeC(ctx2, make(<-chan int))
-	cancel()
-	_, more = <-c7
-	is.True(!more)
-
-	// cancel on write
-	ctx3, cancel := context.WithCancel(context.Background())
-	c8 := make(chan int)
-	c9 := channels.MergeC(ctx3, c8)
-	c8 <- 42
-	cancel()
-	// Potential race? Let's see if it get flaky.
-	_, more = <-c9
-	is.True(!more)
-}
-
 func TestMin(t *testing.T) {
 	is := is.New(t)
 	f := func(given []int, expected int, expectedErr error) {
@@ -497,46 +339,6 @@ func TestMin(t *testing.T) {
 	f([]int{4, 1, 2}, 1, nil)
 	f([]int{1, 2, 4}, 1, nil)
 	f([]int{4, 2, 1}, 1, nil)
-}
-
-func TestPop(t *testing.T) {
-	is := is.New(t)
-
-	// exit on canceled context
-	c := make(chan int)
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-	v, ok := channels.Pop(ctx, c)
-	is.True(!ok)
-	is.Equal(v, 0)
-
-	// succesful read
-	ctx = context.Background()
-	go func() { c <- 12 }()
-	v, ok = channels.Pop(ctx, c)
-	is.True(ok)
-	is.Equal(v, 12)
-
-	// exit on closed channel
-	ctx = context.Background()
-	close(c)
-	v, ok = channels.Pop(ctx, c)
-	is.True(!ok)
-	is.Equal(v, 0)
-}
-
-func TestPush(t *testing.T) {
-	// exit on canceled context
-	c := make(chan int)
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-	channels.Push(ctx, c, 11)
-
-	// push into the channel when there is someone to read
-	ctx = context.Background()
-	go channels.Push(ctx, c, 12)
-	is := is.New(t)
-	is.Equal(<-c, 12)
 }
 
 func TestReduce(t *testing.T) {
@@ -681,68 +483,5 @@ func TestWithBuffer(t *testing.T) {
 
 	close(c1)
 	_, more := <-c1
-	is.True(!more)
-}
-
-// WithContext should echo everything from the input channel
-// and close the resulting channel when the input one is closed.
-func TestWithContext_NoCancellation(t *testing.T) {
-	is := is.New(t)
-	c1 := make(chan int)
-	c2 := channels.WithContext(c1, context.Background())
-
-	r := make([]int, 0)
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := range c2 {
-			r = append(r, i)
-		}
-	}()
-
-	c1 <- 11
-	c1 <- 12
-	c1 <- 13
-	close(c1)
-	wg.Wait()
-	is.Equal(r, []int{11, 12, 13})
-}
-
-// WithContext should exit on cancellation.
-func TestWithContext_Cancellation(t *testing.T) {
-	is := is.New(t)
-	c1 := make(chan int)
-	ctx, cancel := context.WithCancel(context.Background())
-	c2 := channels.WithContext(c1, ctx)
-
-	r := make([]int, 0)
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := range c2 {
-			r = append(r, i)
-			if i == 12 {
-				cancel()
-			}
-		}
-	}()
-
-	c1 <- 11
-	c1 <- 12
-	c1 <- 13
-	wg.Wait()
-	is.Equal(r, []int{11, 12})
-	close(c1)
-}
-
-func TestWithContext_CancelWaitingInput(t *testing.T) {
-	is := is.New(t)
-	c1 := make(chan int)
-	ctx, cancel := context.WithCancel(context.Background())
-	c2 := channels.WithContext(c1, ctx)
-	cancel()
-	_, more := <-c2
 	is.True(!more)
 }
